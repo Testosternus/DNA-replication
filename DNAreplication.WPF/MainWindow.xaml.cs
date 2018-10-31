62f5524cc3700ca4af57e63b0cfbdc7d8fc66270
Jonathan Boydens <jonathan_boydens@hotmail.com>
2018-10-31 17:34:38 +0100

    Documentation
    - del: cleaned & sorted usings
    - mod: short summary description

--- DNAreplication.WPF/MainWindow.xaml.cs
+++ DNAreplication.WPF/MainWindow.xaml.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DNAreplication.WPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// This is the main interaction with the application. 
+    /// No new libraries except the integrated ones were used.
     /// </summary>
     public partial class MainWindow : Window
     {
