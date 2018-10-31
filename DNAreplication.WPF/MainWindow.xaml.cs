741fdb261381eceee0949096af72cf095b9b2908
Jonathan Boydens <jonathan_boydens@hotmail.com>
2018-10-31 16:58:30 +0100

    Generate complement
    - add: <string>RemoveUnwantedCharacters(string in, IEnumerable<char> allowed) : removes the allowed characters from the inputstring
    - add: Main click event on button: filters chars, creates new string with complement chars, formats output
    - mod: txtInput maxlength on 50
    - mod: removed borders from labels
    - mod: grey background on output

--- DNAreplication.WPF/MainWindow.xaml.cs
+++ DNAreplication.WPF/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace DNAreplication.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        string allowedBases = "ATCG";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnComplement_Click(object sender, RoutedEventArgs e)
+        {
+            string inputSeq = RemoveUnwantedCharacters(txtInput.Text.ToUpper(), allowedBases);
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in inputSeq)
+            {
+                switch(c)
+                {
+                    case 'A':
+                        sb.Append('T');
+                        break;
+                    case 'T':
+                        sb.Append('A');
+                        break;
+                    case 'C':
+                        sb.Append('G');
+                        break;
+                    case 'G':
+                        sb.Append('C');
+                        break;
+                    default:
+                        break;
+                }
+            }
+            string inputSeqDisp = (inputSeq.Length == 0) ? ">> input not valid" : inputSeq.ToUpper();
+            string outputSeqDisp = (sb.Length == 0) ? ">> N/A" : sb.ToString();
+
+            tblOutput.Text = $"{inputSeqDisp}\r\n{outputSeqDisp}";
+            txtInput.Clear();
+        }
+
+        private string RemoveUnwantedCharacters(string input, IEnumerable<char> allowedCharacters)
+        {
+            var filtered = input //no need for .ToCharArray() => string is already IEnumerable<char>
+                .Where(c => allowedCharacters.Contains(c)) //where any char in input is part of allowedCharacters
+                .ToArray();
+
+            return new string(filtered);
         }
     }
 }
